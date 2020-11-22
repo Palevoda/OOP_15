be0701bd05ad7f30f6eeec81ef8a3171b7309618
endlesslydivided <sashakovalev2002@hotmail.com>
2020-11-22 13:11:06 +0300

    Добавьте файлы проекта.

---
+++ OOP_15/LAB_METHODS.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using static System.Console;
+using System.Threading;
+using System.IO;
+using System.Reflection;
+
+namespace OOP_15
+{
+    public static class LAB_METHODS
+    {
+        #region FIELDS
+        static object locker = new object();
+        public static StreamWriter fstream = new StreamWriter(@"..\Numbers_1.txt");
+        public static Mutex mutex = new Mutex();
+        #endregion
+
+        #region METHODS
+        public static void ProcessAnalyze()
+        {
+            List<Process> processes_list = new List<Process>(Process.GetProcesses());
+            using (StreamWriter fstream = new StreamWriter(@"..\FileOut.txt"))
+            {
+                foreach (Process item in processes_list)
+                {
+                    WriteLine($"\n ID процесса:{item.Id}");
+                    WriteLine("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                    WriteLine($"Имя процесса:{item.ProcessName}");
+                    WriteLine($"Приоритет процесса:{item.BasePriority}");
+
+
+                    fstream.WriteLine($"\n ID процесса:{item.Id}");
+                    fstream.WriteLine("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+
+                    try
+                    {
+                        WriteLine($"Время запуска процесса:{item.StartTime}");
+                        WriteLine($"Время использования CPU: {DateTime.Now.Hour - item.StartTime.Hour}:" +
+                            $"{(DateTime.Now.Minute > item.StartTime.Minute ? DateTime.Now.Minute - item.StartTime.Minute : item.StartTime.Minute - DateTime.Now.Minute)}:" +
+                            $"{(DateTime.Now.Second > item.StartTime.Second ? DateTime.Now.Second - item.StartTime.Second : item.StartTime.Second - DateTime.Now.Second)}");
+
+                        fstream.WriteLine($"Время запуска процесса:{item.StartTime}");
+                        fstream.WriteLine($"Время использования CPU: {DateTime.Now.Hour - item.StartTime.Hour}:" +
+                           $"{(DateTime.Now.Minute > item.StartTime.Minute ? DateTime.Now.Minute - item.StartTime.Minute : item.StartTime.Minute - DateTime.Now.Minute)}:" +
+                           $"{(DateTime.Now.Second > item.StartTime.Second ? DateTime.Now.Second - item.StartTime.Second : item.StartTime.Second - DateTime.Now.Second)}");
+                    }
+                    catch (Exception ex)
+                    {
+                        WriteLine(ex.Message);
+                    }
+
+                    fstream.WriteLine($"Имя процесса:{item.ProcessName}");
+                    fstream.WriteLine($"Приоритет процесса:{item.BasePriority}");
+                    fstream.WriteLine("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+
+                    WriteLine("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                }
+                Process notepad = Process.Start("notepad", @"..\FileOut.txt");
+                Console.ReadKey();
+                notepad.Kill();
+            }
+        }
+        public static void DomainAnalyze()
+        {
+            AppDomain CurrentDomain = AppDomain.CurrentDomain;
+            WriteLine($"Имя домена: {CurrentDomain.FriendlyName}");
+            WriteLine($"Детали конфигурации: имя приложения:{CurrentDomain.SetupInformation.ApplicationName,10};" +
+                $"\n каталог приложения :{CurrentDomain.SetupInformation.ApplicationBase,5};");
+            WriteLine("Все сборки:");
+            foreach (Assembly item in CurrentDomain.GetAssemblies())
+            {
+                WriteLine(item);
+            }
+
+            AppDomain NewOne = AppDomain.CreateDomain("notepad");
+            var domain = Thread.GetDomain();
+            NewOne.Load(domain.GetAssemblies()[0].FullName);
+            AppDomain.Unload(NewOne);
+        }
+        public static void ThreadAnalyze()
+        {
+            Thread thread = Thread.CurrentThread;
+            int n;
+            Console.Write("Введите значение n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Thread.Sleep(100);
+                Console.Write(" " + i);
+
+                if (i % 5 == 0)
+                {
+                    thread.Suspend();
+                }
+            }
+            WriteLine($"\nСтатус потока:{thread.ThreadState}");
+            WriteLine($"Имя потока:{thread.Name}");
+            WriteLine($"Приоритет потока:{thread.Priority}");
+            WriteLine($"ID потока:{thread.ManagedThreadId}");
+            thread.Interrupt();
+            WriteLine();
+        }
+        public static void Timer(object a)
+        {
+            WriteLine("\n▬▬▬▬▬▬▬▬▬ Сработал таймер ▬▬▬▬▬▬▬▬▬\n");
+        }
+        public static void OutputNumbers(object inputN)
+        {
+            lock (locker)
+            {
+                int n = (int)inputN;
+                if (Thread.CurrentThread.Name == "1")
+                    for (int i = 1; i <= n; i++)
+                        if (i % 2 != 0)
+                        {
+                            fstream.Write($" {i,3} ");
+                            Write($" {i,3} ");
+                        }
+
+                if (Thread.CurrentThread.Name == "2")
+                    for (int i = 1; i <= n; i++)
+                        if (i % 2 == 0)
+                        {
+                            fstream.Write($" {i,3} ");
+                            Write($" {i,3} ");
+                        }
+            }
+
+        }
+        public static void OutputNumbersSinh(object inputN)
+        {
+            int n = (int)inputN;
+            if (Thread.CurrentThread.Name == "1")
+            {
+                for (int i = 1; i <= n; i++)
+                    if (i % 2 != 0)
+                    {
+                        mutex.WaitOne();
+                        fstream.Write($" {i,3} ");
+                        Write($" {i,3} ");
+                        mutex.ReleaseMutex();
+                    }
+            }
+
+            if (Thread.CurrentThread.Name == "2")
+            {
+                for (int i = 1; i <= n; i++)
+                    if (i % 2 == 0)
+                    {
+                        mutex.WaitOne();
+                        fstream.Write($" {i,3} ");
+                        Write($" {i,3} ");
+                        mutex.ReleaseMutex();
+                    }
+            }
+
+        }
+        #endregion
+
+    }
+}
---
+++ OOP_15/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Console;
+using System.Threading;
+using static OOP_15.LAB_METHODS;
+
+namespace OOP_15
+{
+    class Program
+    {
+ 
+        static void Main()
+        {
+            #region PROCESS
+            ProcessAnalyze();
+            #endregion
+
+            #region DOMAIN
+            DomainAnalyze();
+            #endregion
+
+            #region TIMER & THREAD
+            TimerCallback tmr = new TimerCallback(Timer);
+            Timer timer = new Timer(tmr, null, 0, 5000);
+
+            Thread thread = new Thread(new ThreadStart(ThreadAnalyze)) { Name = "Имя_потока", Priority = ThreadPriority.Highest };
+            thread.Start();
+
+            while (thread.ThreadState != System.Threading.ThreadState.Stopped)
+            {
+                if (thread.ThreadState == System.Threading.ThreadState.Suspended)
+                {
+                    Thread.Sleep(500);
+                    thread.Resume();
+                }
+            }
+            #endregion
+
+            #region EVEN & ODD
+            Thread thread1 = new Thread(new ParameterizedThreadStart(OutputNumbers)) { Name = "1", Priority = ThreadPriority.Highest };
+            Thread thread2 = new Thread(new ParameterizedThreadStart(OutputNumbers)) { Name = "2", Priority = ThreadPriority.AboveNormal };
+            thread1.Start(15);
+            thread2.Start(15);
+
+            while (true)
+            {
+                if (thread1.IsAlive == false && thread2.IsAlive == false)
+                {
+                    fstream.WriteLine("\n══════════════════════");
+                    WriteLine("\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                    Thread thread3 = new Thread(new ParameterizedThreadStart(OutputNumbersSinh)) { Name = "1" };
+                    Thread thread4 = new Thread(new ParameterizedThreadStart(OutputNumbersSinh)) { Name = "2" };
+                    thread3.Start(15);
+                    thread4.Start(15);
+                    while (true)
+                    {
+                        if (thread1.IsAlive == false && thread2.IsAlive == false && thread3.IsAlive == false && thread4.IsAlive == false)
+                        {
+                            fstream.Close();
+                            break;
+                        }
+                    }
+                    break;
+                }
+               
+            }
+            
+            Process open_note =Process.Start("notepad", @"..\Numbers_1");
+            ReadKey();
+            if(open_note.HasExited != true)
+            open_note.Kill();
+            #endregion
+
+            Console.ReadKey();
+        }
+
+       
+    }
+}
